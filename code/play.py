aa5c440cdd20f6a9dbf82f6b1d037e90fcf362bf
Gordon <gordonbchen@gmail.com>
2023-07-11 17:11:53 -0400

    use minimax tree with nodes

--- code/play.py
+++ code/play.py
@@ -1,64 +1,77 @@
+from __future__ import annotations
+
 import numpy as np
+
+from typing import Union, List
 
 from tic_tac_toe import Board, Codes, Move
 
 
-def get_eval(state: np.ndarray, use_max: bool) -> int:
-    """Get the state evaluation using minimax."""
+class Node:
+    """A minimax node storing an eval and children nodes."""
+
+    def __init__(self, state: np.ndarray, eval: int, children: Union[None, List[Node]], use_max: bool) -> None:
+        """Create the node."""
+        self.state = state
+        self.eval = eval
+        self.children = children
+        self.use_max = use_max
+
+    def get_best_move(self) -> Move:
+        """Get the best move at the current state."""
+        # Find best eval.
+        poss_evals = [child.eval for child in self.children]
+        best_eval = max(poss_evals) if self.use_max is True else min(poss_evals)
+        best_eval_ind = poss_evals.index(best_eval)
+
+        # Find state with best eval.
+        poss_states = [child.state for child in self.children]
+        best_state = poss_states[best_eval_ind]
+
+        # Find move getting best state.
+        best_move = Board.get_move(self.state, best_state)
+        return best_move
+
+    def get_child(self, state) -> Node:
+        """Get the child with the given state."""
+        for child in self.children:
+            if np.all(child.state == state):
+                return child
+
+
+def create_minimax_tree(state: np.ndarray, use_max: bool) -> Node:
+    """Create a minmax tree."""
     # Base cases of win and tie.
     winner = Board.check_win(state)
     if winner is not None:
         # Marks correspond to score: x = 1, o = -1.
-        return winner
+        node = Node(state, eval=winner, children=None, use_max=(not use_max))
+        return node
     elif Board.check_tie(state):
         # Tie corresponds to 0 score.
-        return Codes.TIE
+        node = Node(state, 0, children=None, use_max=(not use_max))
+        return node
 
-    # Get possible children evals.
-    move_state_map = Board.get_move_state_map(state)
+    # Recursively get eval of children nodes.
+    poss_states = Board.get_poss_states(state)
+    children = [create_minimax_tree(poss_state, use_max=(not use_max)) for poss_state in poss_states]
 
-    # Recursively get eval of children states.
-    poss_states = move_state_map.values()
-    poss_evals = [
-        get_eval(poss_state, use_max=(not use_max)) for poss_state in poss_states
-    ]
+    # Get eval from children.
+    poss_evals = [child.eval for child in children]
+    best_eval = max(poss_evals) if use_max is True else min(poss_evals)
 
-    # Return min or max.
-    if use_max is True:
-        return max(poss_evals)
-    else:
-        return min(poss_evals)
+    # Create node.
+    node = Node(state, best_eval, children, use_max=(use_max))
+    return node
 
 
-def get_best_move(state: np.ndarray, use_max: bool) -> Move:
-    """Get the best move at the state."""
-    # Get moves and corresponding states.
-    move_state_map = Board.get_move_state_map(state)
-    poss_moves = list(move_state_map.keys())
-    poss_states = move_state_map.values()
-
-    # Get evals of children.
-    poss_evals = [
-        get_eval(poss_state, use_max=(not use_max)) for poss_state in poss_states
-    ]
-
-    # Get best state index.
-    if use_max is True:
-        best_eval = max(poss_evals)
-    else:
-        best_eval = min(poss_evals)
-
-    best_state_ind = poss_evals.index(best_eval)
-
-    # Map best state back to best move.
-    best_move = poss_moves[best_state_ind]
-    return best_move
-
-
-def play_game(human_first: bool) -> None:
+def play_game() -> None:
     """Play a game against minimax."""
     # Create new board.
     board = Board()
+
+    # Create minimax tree.
+    node = create_minimax_tree(board.state, use_max=True)
 
     # Play game for 9 turns (max).
     for n_turn in range(1, 10):
@@ -66,20 +79,20 @@ def play_game(human_first: bool) -> None:
         print(f"\nTurn {n_turn}:")
         print(board.state)
 
-        if n_turn % 2 == human_first:
+        if n_turn % 2 == 1:
             # Human player turn.
-            n_row = int(input("Row: "))
-            n_col = int(input("Col: "))
+            n_row, n_col = (int(char) for char in input("Row, Col: ").split(", "))
             move = Move(n_row, n_col)
 
-            # Make move.
             board.make_move(move)
         else:
             # Computer player turn.
-            move = get_best_move(board.state, use_max=(human_first is False))
+            move = node.get_best_move()
 
-            # Make move.
             board.make_move(move)
+
+        # Update node.
+        node = node.get_child(board.state)
 
         # Show board.
         print(board.state)
@@ -98,7 +111,5 @@ def play_game(human_first: bool) -> None:
 
 
 if __name__ == "__main__":
-    # Play the game.
-    for i in range(3):
-        human_first = i % 2 == 1
-        play_game(human_first)
+    # Play tic-tac-toe.
+    play_game()
